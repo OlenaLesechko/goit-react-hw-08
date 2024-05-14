a8cfcd498d8d40d8e813fada52e759b4ca309c60
OlenaLesechko <141057041+OlenaLesechko@users.noreply.github.com>
2024-05-14 22:05:34 +0200

    start
    перенесла коди з файлів

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://663d1bb517145c4d8c38c142.mockapi.io/';
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 
 export const fetchContacts = createAsyncThunk(
     'contacts/fetchAll',
