c94cfaa982bfdc7c1a88e58adf41082d14328e95
OlenaLesechko <141057041+OlenaLesechko@users.noreply.github.com>
2024-05-15 12:29:49 +0200

    upd components and pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Title from '../components/Title/Title';
+/* import React from 'react';
+import css from './Home.module.css'; */
+
+
+export default function Home() {
+    return (
+        <>
+            <Title />
+
+        </>
+    );
+}
